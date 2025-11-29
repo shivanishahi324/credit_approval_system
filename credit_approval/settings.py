a200fce05c98729c810083852a6e00bbab517c25
Shivani Shahi <shivanishahi324@gmail.com>
2025-11-29 09:48:15 +0530

    Updated settings for Render DB

--- credit_approval/settings.py
+++ credit_approval/settings.py
@@ -69,13 +69,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'credit_approval.wsgi.application'
 
 # -------------------- DATABASE --------------------
-# Use Render PostgreSQL (MUST)
+
+import os
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True,
-    )
+    'default': dj_database_url.config(default=os.environ["DATABASE_URL"], conn_max_age=600)
 }
 
 # -------------------- PASSWORD VALIDATION --------------------
